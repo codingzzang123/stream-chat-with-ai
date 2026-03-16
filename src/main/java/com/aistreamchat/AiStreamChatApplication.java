46ad7545cca0710a0f15072235d8f7e3a01a16e6
codingzzang123 <hosun9752@gmail.com>
2026-03-17 00:10:48 +0900

    initial commit

---
+++ src/main/java/com/aistreamchat/AiStreamChatApplication.java
@@ -0,0 +1,13 @@
+package com.aistreamchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiStreamChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiStreamChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aistreamchat/AiStreamChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aistreamchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiStreamChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
